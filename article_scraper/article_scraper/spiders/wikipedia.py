3fe52a9b67bc5b9114a0bcc9e096f05e5121edb6
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-04 12:02:15 +0300

    Create Article class in article_scraper/items.py

--- article_scraper/article_scraper/spiders/wikipedia.py
+++ article_scraper/article_scraper/spiders/wikipedia.py
@@ -1,6 +1,7 @@
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from article_scraper.items import Article
 
 
 class WikipediaSpider(CrawlSpider):
@@ -11,8 +12,10 @@ class WikipediaSpider(CrawlSpider):
     rules = [Rule(LinkExtractor(allow=r'wiki/((?!:).)*$'), callback='parse_info', follow=True)]
 
     def parse_info(self, response):
-        return {
-            'title': response.xpath('//h1/text()').get() or response.xpath('//h1/i/text()').get(),
-            'url': response.url,
-            'last_edited': response.xpath('//li[@id="footer-info-lastmod"]/text()').get()
-        }
+
+        article = Article()
+        article['title'] = response.xpath('//h1/text()').get() or response.xpath('//h1/i/text()').get()
+        article['url'] = response.url
+        article['lastUpdated'] = response.xpath('//li[@id="footer-info-lastmod"]/text()').get()
+
+        return article
