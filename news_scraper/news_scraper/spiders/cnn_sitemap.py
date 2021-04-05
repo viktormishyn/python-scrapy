1954be2bd0da991d508df325c54ee4906a498af7
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-05 15:23:42 +0300

    Build cnn_sitemap spider

--- news_scraper/news_scraper/spiders/cnn_sitemap.py
+++ news_scraper/news_scraper/spiders/cnn_sitemap.py
@@ -1,10 +1,20 @@
-import scrapy
+from scrapy.spiders import SitemapSpider
+from news_scraper.items import NewsArticle
 
 
-class CnnSitemapSpider(scrapy.Spider):
-    name = 'cnn_sitemap'
+class CnnSpider(SitemapSpider):
+    name = 'cnn'
     allowed_domains = ['cnn.com']
-    start_urls = ['http://cnn.com/']
+    sitemap_urls = ['https://www.cnn.com/sitemaps/article-2021-04.xml']
 
     def parse(self, response):
-        pass
+        article = NewsArticle()
+        article['url'] = response.url
+        article['source'] = 'CNN'
+        article['title'] = response.xpath('//h1[@class="pg-headline"]/text()').get()
+        article['description'] = response.xpath('//meta[@itemprop="description"]/@content').get()
+        article['body'] = response.xpath('//section[@data-zone-label="bodyText"]/div[@class="l-container"]//*/text()').getall()
+        article['author'] = response.xpath('//meta[@itemprop="author"]/@content').get()
+        article['datePublished'] = response.xpath('//meta[@itemprop="datePublished"]/@content').get()
+
+        return article
