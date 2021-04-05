d69ea202882f48c159bf0dbbc5ebd0f28c72dbf8
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-05 13:08:46 +0300

    Build associated_press spider

--- news_scraper/news_scraper/spiders/associated_press.py
+++ news_scraper/news_scraper/spiders/associated_press.py
@@ -1,10 +1,26 @@
-import scrapy
+# -*- coding: utf-8 -*-
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from news_scraper.items import NewsArticle
+import json
 
 
-class AssociatedPressSpider(scrapy.Spider):
+class AssociatedPressSpider(CrawlSpider):
     name = 'associated_press'
     allowed_domains = ['apnews.com']
-    start_urls = ['http://apnews.com/']
+    start_urls = ['https://apnews.com/']
+    rules = [Rule(LinkExtractor(allow=r'\/article\/[a-zA-Z0-9\-]+'), callback='parse_item', follow=True)]
 
-    def parse(self, response):
-        pass
+    def parse_item(self, response):
+        article = NewsArticle()
+        # <script data-rh="true">
+        article['url'] = response.url
+        article['source'] = 'Associated Press'
+
+        jsonData = json.loads(response.xpath('//script[@data-rh="true"]/text()').get())
+        article['title'] = jsonData['headline']
+        article['description'] = jsonData['description']
+        article['body'] = response.xpath('//div[@class="Article"]/p/text()').getall()
+        article['author'] = jsonData['author'][0]
+        article['datePublished'] = jsonData['datePublished']
+        return article
