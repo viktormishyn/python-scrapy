f323c8a1f73a45ab2273d4d40eb99d56026442bf
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-05 13:11:41 +0300

    Build yahoo spider

--- news_scraper/news_scraper/spiders/yahoo.py
+++ news_scraper/news_scraper/spiders/yahoo.py
@@ -1,10 +1,27 @@
-import scrapy
+# -*- coding: utf-8 -*-
+import json
+from news_scraper.items import NewsArticle
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 
-class YahooSpider(scrapy.Spider):
+class YahooSpider(CrawlSpider):
     name = 'yahoo'
     allowed_domains = ['news.yahoo.com']
     start_urls = ['http://news.yahoo.com/']
+    rules = [Rule(LinkExtractor(allow=r'\/[a-zA-Z\-]+-[0-9]+.html'), callback='parse_item', follow=True)]
 
-    def parse(self, response):
-        pass
+    def parse_item(self, response):
+        article = NewsArticle()
+        article['url'] = response.url
+        article['source'] = 'Yahoo News'
+
+        jsonData = json.loads(
+            response.xpath('//article[@role="article"]/script[@type="application/ld+json"]/text()').get())
+
+        article['title'] = jsonData['headline']
+        article['description'] = jsonData['description']
+        article['body'] = response.xpath('//div[@class="caas-body"]/p/text()').getall()
+        article['author'] = jsonData['author']['name']
+        article['datePublished'] = jsonData['datePublished']
+        return article
