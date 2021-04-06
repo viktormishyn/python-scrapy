d0918bff31d5e64ba012c53b275bf6bcc26b15bb
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-06 12:19:15 +0300

    Add cnn_all_articles.py

--- news_scraper/news_scraper/spiders/cnn_all_articles.py
+++ news_scraper/news_scraper/spiders/cnn_all_articles.py
@@ -1,10 +1,28 @@
-import scrapy
+# -*- coding: utf-8 -*-
+from scrapy.spiders import CrawlSpider, Rule
 
 
-class CnnAllArticlesSpider(scrapy.Spider):
+# not working solution (the reason is unclear, worked in the late 2020 - 2.4.0 ???)
+
+def generate_start_urls():
+    years = ['2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021']
+    months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+    return ['https://www.cnn.com/sitemaps/article-{}-{}.xml'.format(year, month) for year in years for month in months]
+
+
+class CnnSpider(CrawlSpider):
     name = 'cnn_all_articles'
     allowed_domains = ['cnn.com']
-    start_urls = ['http://cnn.com/']
+    start_urls = generate_start_urls()
+
+    custom_settings = {
+        'CLOSESPIDER_PAGECOUNT': 0,
+        'ROBOTSTXT_OBEY': False,
+        'FEED_URI': 'count.csv',
+        'FEED_FORMAT': 'csv',
+        'ITEM_PIPELINES': {}
+
+    }
 
     def parse(self, response):
-        pass
+        return {'url': response.url, 'count': response.text.count('<url>')}
